2cdbbfb212d4eb4fc5a983e8fb9845cf7cc56815
Babe Shark <xratedrbw@gmail.com>
2022-01-07 14:04:23 -0700

    adding in models for event and eventdata

--- sauroneye/models.py
+++ sauroneye/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class Event(models.Model):
+    # unique id key
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    session_id = models.CharField(max_length=200)
+    category = models.CharField(max_length=500)
+    name = models.CharField(max_length=200)
+    timestamp = models.DateTimeField(null=True)
+
+class EventData(models.Model):
+    session = models.ForeignKey(Event, on_delete=models.CASCADE)
+    host = models.CharField(max_length=500, null=False, blank=True)
+    path = models.CharField(max_length=500, null=False, blank=True)
--- sauroneye/views.py
+++ sauroneye/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from mordor.sauroneye.serializers import EventSerializer
+from .models import Event, EventData
+from rest_framework import generics,response,status
+
+class SauronEyeAPI(generics.GenericAPIView):
+    serialer_class = EventSerializer
+
+    def post(self, request):
+
+def sauroneye(request, pk):
+
+    if request.method == "POST":
+
+
+
